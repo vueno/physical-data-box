f507825ff3d4211201f671f2e6895c0db15451ac
DavidBueno <dpb2235@gmail.com>
2016-02-02 01:08:36 +0100

    Arduino_code

---
+++ RFID_Kartennummer.ino
@@ -0,0 +1,40 @@
+// TIMESTAMP verwenden!
+#include <SPI.h>
+#include <MFRC522.h>
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup(){
+Serial.begin(9600);
+SPI.begin();
+mfrc522.PCD_Init();
+pinMode (2, OUTPUT);
+}
+
+void loop(){
+if ( ! mfrc522.PICC_IsNewCardPresent()){
+return;
+}
+
+ 
+if ( ! mfrc522.PICC_ReadCardSerial()){
+return;
+}
+
+long code=0;
+for (byte i = 0; i < mfrc522.uid.size; i++){
+
+code=((code+mfrc522.uid.uidByte[i])*10);
+
+}
+
+//Serial.print("Die Kartennummer lautet:");
+Serial.println(code);
+
+if (code==1031720){
+digitalWrite (2, HIGH);
+delay (5000);
+digitalWrite (2, LOW);
+}
+  }
